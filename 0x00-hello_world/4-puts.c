787e1c4ca259482e99be21bd705ba230db962b7b
maryam-ebrahim <maryamebrahem484@gmail.com>
2023-02-16 15:50:17 -0800

    C course

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - A program that prints a line with puts function
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
-	puts ("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
