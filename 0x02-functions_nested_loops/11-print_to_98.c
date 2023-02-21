30b5c8fb8d6d0987f8abba96bb7843778f862a1c
maryam-ebrahim <maryamebrahem484@gmail.com>
2023-02-21 15:36:40 -0800

    0x02

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,8 +15,8 @@ void print_to_98(int n)
 	{
 	if (n == 98)
 	{
-	_putchar(n + '0');
-	_putchar('\n');
+	printf("%d", n);
+	printf("\n");
 	break;
 	}
 	else
@@ -31,8 +31,8 @@ void print_to_98(int n)
 	{
 	if (n == 98)
 	{
-	_putchar(n + '0');
-	_putchar('\n');
+	printf("%d", n);
+	printf("\n");
 	break;
 	}
 	else
