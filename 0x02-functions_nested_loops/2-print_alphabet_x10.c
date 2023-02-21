a38ddb5cd5dca03cc6f1904e84466002834c1346
maryam-ebrahim <maryamebrahem484@gmail.com>
2023-02-21 13:02:20 -0800

    0x02

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,19 +9,17 @@
 void print_alphabet_x10(void)
 {
 	char c;
-	int j;
+	int i;
 
-	c = 'a';
-	j = 0;
-	while (j < 10)
+	i = 0;
+
+	while (i < 10)
 	{
-		while (c <= 'z')
+		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
-			c++;
 		}
-
 		_putchar('\n');
-		j++;
+		i++;
 	}
 }
