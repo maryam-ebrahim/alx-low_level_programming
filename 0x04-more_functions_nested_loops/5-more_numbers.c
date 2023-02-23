4cbf1fff61c1ca5ed79d36cdc8b04690223fa7f5
maryam-ebrahim <maryamebrahem484@gmail.com>
2023-02-23 15:53:13 -0800

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -19,9 +19,9 @@ void more_numbers(void)
 		{
 			if (i >= 10)
 			{
-				_putchar(i - '0');
+				_putchar((i /10) + '0');
 			}
-			_putchar(i + '0');
+			_putchar((i % 10) + '0');
 		}
 		_putchar('\n');
 	}
