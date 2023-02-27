b598a31fa2e72def54de66aacfc306df605d1c0b
maryam-ebrahim <maryamebrahem484@gmail.com>
2023-02-27 23:14:35 +0200

    task 1

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,8 +3,8 @@
 
 /**
  * reset_to_98 - A function that updates the value it points to to 98
- * @*n: the pointer to be updated
- * Return: 0 (success)
+ * @n: the pointer to be updated
+ * Return: nothing
 */
 
 void reset_to_98(int *n)
