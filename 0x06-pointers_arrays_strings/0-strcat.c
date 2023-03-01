b53610c750ed985ad6421e6ab5700137d5e9bfa0
maryam-ebrahim <maryamebrahem484@gmail.com>
2023-03-01 13:20:08 -0800

    task 1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,6 +11,18 @@
 
 char *_strcat(char *dest, char *src)
 {       
-	strcat(dest, src);
+	int index, dest_len;
+
+	index = 0;
+	dest_len = 0;
+
+	while (dest[index++])
+	{
+		dest_len++;
+	}
+	for (index = 0; src[index] && index < n; index++)
+	{
+		dest[dest_len++] = src[index];
+	}
 	return (dest);
 }
