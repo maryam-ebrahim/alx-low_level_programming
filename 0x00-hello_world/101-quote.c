14c5aca9570c8b4aa1958ee6cf1bfbc7b2737067
maryam-ebrahim <maryamebrahem484@gmail.com>
2023-03-05 05:26:21 -0800

    task 8 advanced

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints a phrase
+ *
+ * Return: 1 when success
+ */
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
