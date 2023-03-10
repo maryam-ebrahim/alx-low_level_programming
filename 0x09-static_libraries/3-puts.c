b2d19aabde4c85df33f74fc501d0ec3e1eebabeb
maryam-ebrahim <maryamebrahem484@gmail.com>
2023-03-10 14:53:52 -0800

    task 1

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,10 +1,10 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
-/**
- * _puts - A function that prints a string
- * Return: 0 (success)
- */
-
-void _puts(char *str)
-{}
+void _puts(char *s)
+{
+	puts(s);
+}
