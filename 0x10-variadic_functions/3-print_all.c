d85883788ea3be78ecde8034cb0b2aa542e50112
maryam-ebrahim <maryamebrahem484@gmail.com>
2023-03-23 16:22:38 -0700

    all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -14,7 +14,6 @@ void print_all(const char * const format, ...)
 	va_list list;
 
 	va_start(list, format);
-
 	if (format)
 	{
 		while (format[i])
@@ -24,15 +23,12 @@ void print_all(const char * const format, ...)
 				case 'c':
 					printf("%s%c", sep, va_arg(list, int));
 					break;
-
 				case 'i':
 					printf("%s%d", sep, va_arg(list, int));
 					break;
-
 				case 'f':
 					printf("%s%f", sep, va_arg(list, double));
 					break;
-
 				case 's':
 					str = va_arg(list, char *);
 					if (!str)
@@ -47,7 +43,6 @@ void print_all(const char * const format, ...)
 			i++;
 		}
 	}
-
 	printf("\n");
 	va_end(list);
 }
